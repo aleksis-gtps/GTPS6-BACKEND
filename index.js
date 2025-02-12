d68ab74c361f97d779f3f8bedfacabc12bb1a8b2
aleksis <alubinskis0@gmail.com>
2025-02-12 20:24:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.all('/player/growid/checktoken', function (req, res) {
     try {
         const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
         for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
-        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
+        if (uName[1] && uPass[1]) { res.redirect('/database/players'); }
     } catch (why) { console.log(`Warning: ${why}`); }
 
     res.render(__dirname + '/public/html/dashboard.ejs', { data: tData });
