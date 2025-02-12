99f0487f277910f2b1c7b9d7302ce71ade546bea
aleksis <alubinskis0@gmail.com>
2025-02-12 20:10:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,9 @@ app.all('/player/growid/login/validate', (req, res) => {
 
 app.all('/player/growid/checktoken', function (req, res) {
     const _token = req.body._token;
+     const growId = req.body.growId;
+    const password = req.body.password;
+    
     const token = Buffer.from(
         `_token=${_token}&growId=${growId}&password=${password}`,
     ).toString('base64');
